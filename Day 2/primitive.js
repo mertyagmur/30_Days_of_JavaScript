62d2e29e4631c24c9e91515662c34050255be454
mertyagmur <mert.yagmurcs@gmail.com>
2023-04-24 21:13:50 +0300

    Add Day 2 files

---
+++ Day 2/primitive.js
@@ -0,0 +1,36 @@
+let numOne = 3
+let numTwo = 3
+
+console.log(numOne == numTwo)
+
+let js = "JavaScript"
+let py = "Python"
+
+console.log(js == py)
+
+let lightOn = true
+let lightOff = false
+
+console.log(lightOn == lightOff)
+
+/* let nums = [1, 2, 3]
+nums[0] = 10
+console.log(nums) */
+
+let nums = [1, 2, 3]
+let numbers = [1, 2, 3]
+console.log(nums == numbers)
+
+let userOne = {
+    name:'Asabeneh',
+    role:'teaching',
+    country:'Finland'
+}
+    
+let userTwo = {
+    name:'Asabeneh',
+    role:'teaching',
+    country:'Finland'
+}
+    
+console.log(userOne == userTwo)
