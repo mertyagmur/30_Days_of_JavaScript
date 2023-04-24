96bbf4f32e08073c6df61384fe97d9e41b82d5d1
Mert Yağmur <mert.yagmurcs@gmail.com>
2023-04-25 01:59:49 +0300

    Start Day 3

---
+++ Day 3/main.js
@@ -0,0 +1,5 @@
+let promptText = prompt('required text', 'optional text')
+console.log(promptText)
+
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) // result will be true or false based on what you click on the dialog box
