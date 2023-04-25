ca10b311f2adb7cb91e911a859ae35d4442a998e
Mert Yağmur <mert.yagmurcs@gmail.com>
2023-04-25 03:19:28 +0300

    Finish Day 3 Level 2 exercises

---
+++ Day 3/exercises.js
@@ -0,0 +1,20 @@
+// Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = "Mert"
+let lastName = "Yağmur"
+let country = "Turkey"
+let city = "Istanbul"
+let age = 24
+let isMarried = false
+let year = 2024
+
+console.log(typeof firstName)
+console.log(typeof age)
+console.log(typeof isMarried)
+
+// Check if type of '10' is equal to 10
+console.log(typeof "10" == typeof 10)
+
+// Check if parseInt('9.8') is equal to 10
+console.log(parseInt("9.8") == 10)
+
+console.log(4 === "4")
--- Day 3/main.js
+++ Day 3/main.js
@@ -1,5 +1,9 @@
-let promptText = prompt('required text', 'optional text')
-console.log(promptText)
+//let promptText = prompt('required text', 'optional text')
+//console.log(promptText)
 
-const agree = confirm('Are you sure you like to delete? ')
-console.log(agree) // result will be true or false based on what you click on the dialog box
+//const agree = confirm('Are you sure you like to delete? ')
+//console.log(agree) // result will be true or false based on what you click on the dialog box
+
+const now = new Date()
+console.log(now)
+console.log(now.getHours())
